f3a00c5b11ff49fbd0b3470e246795fa2fdcb03b
amannocci <adrien.mannocci@gmail.com>
2016-07-05 17:02:28 +0200

    [Fixed] Ensure default case

--- public/metric_percent_vis_controller.js
+++ public/metric_percent_vis_controller.js
@@ -6,6 +6,10 @@ define(function (require) {
 
     const metrics = $scope.metrics = [];
 
+    function isInvalid(val) {
+      return _.isUndefined(val) || _.isNull(val) || _.isNaN(val);
+    }
+
     $scope.processTableGroups = function (tableGroups) {
       tableGroups.tables.forEach(function (table) {
         var base = null;
@@ -24,6 +28,7 @@ define(function (require) {
             if (row[0] !== '_global') {
               var column = row[0];
               var value = row[1];
+              value = isInvalid(value) ? '?' : value;
 
               metrics.push({
                 label: column,
@@ -33,13 +38,6 @@ define(function (require) {
             }
           });
         }
-
-        if (metrics.length == 0) {
-          metrics.push({
-            label: "No Result",
-            value: "?"
-          })
-        }
       });
     };
 
@@ -47,6 +45,12 @@ define(function (require) {
       if (resp) {
         metrics.length = 0;
         $scope.processTableGroups(tabifyAggResponse($scope.vis, resp));
+        if (metrics.length == 0) {
+          metrics.push({
+            label: "No Result",
+            value: "?"
+          })
+        }
       }
     });
 
