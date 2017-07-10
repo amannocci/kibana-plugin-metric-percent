43cc7ae9fc1258172c92312594a239acacee0bff
fbaligand <fbaligand@gmail.com>
2017-07-10 12:17:07 +0200

    Add support for Kibana 5.5.0

--- public/metric_percent_vis.js
+++ public/metric_percent_vis.js
@@ -1,3 +1,8 @@
+import { VisTypesRegistryProvider } from 'ui/registry/vis_types';
+import { TemplateVisTypeProvider } from 'ui/template_vis_type/template_vis_type';
+import { VisSchemasProvider } from 'ui/vis/schemas';
+
+
 define(function (require) {
 
   // we need to load the css ourselves
@@ -7,11 +12,11 @@ define(function (require) {
   require('plugins/metric_percent_vis/metric_percent_vis_controller');
 
   // register the provider with the visTypes registry
-  require('ui/registry/vis_types').register(MetricVisProvider);
+  VisTypesRegistryProvider.register(MetricVisProvider);
 
   function MetricVisProvider(Private) {
-    var TemplateVisType = Private(require('ui/template_vis_type/template_vis_type'));
-    var Schemas = Private(require('ui/vis/schemas'));
+    const TemplateVisType = Private(TemplateVisTypeProvider);
+    const Schemas = Private(VisSchemasProvider);
 
     // return the visType object, which kibana will use to display and configure new
     // Vis object of this type.
