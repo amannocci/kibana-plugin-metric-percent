642befbc11cb4a256f94ad9e709aa0d1ce3b941f
amannocci <adrien.mannocci@gmail.com>
2018-01-28 00:18:30 +0100

    [Fixed] Correct plugin packaging
    Signed-off-by: amannocci <adrien.mannocci@gmail.com>

--- scripts/setup-plugin.sh
+++ scripts/setup-plugin.sh
@@ -18,6 +18,9 @@ function is_install {
   fi
 }
 
+# Base directory
+BASE=$PWD
+
 # Check dependencies
 is_install "git"
 is_install "zip"
@@ -32,35 +35,33 @@ info "Repository setup to: $URL"
 info "Kibana version: (default: master)"
 read -p "> " VERSION
 VERSION=${VERSION:-master}
+info "Kibana version setup to: $VERSION"
 
 # Clone plugin
-info "Cloning plugin in current directory"
+info "Cloning plugin in tmp"
 git clone $URL /tmp/kibana-plugin-metric-percent
 if [ $? -ne 0 ]; then
   error "The repository can't be used"
   exit 1
-else
-  info "Cloning plugin in current directory"
 fi
 
 if [ "$VERSION" != "master" ]; then
   cd /tmp/kibana-plugin-metric-percent
+  info "Checkout to the right version of plugin for Kibana $VERSION"
   git checkout tags/$VERSION
 
   if [ $? -ne 0 ]; then
     error "This plugin isn't supported for Kibana $VERSION"
     exit 1
-  else
-    info "Checkout to the right version of plugin for Kibana $VERSION"
   fi
 fi
-cd /tmp
 
 # Repacking plugin
 info "Repacking plugin..."
-mkdir -p /tmp/kibana/metric_percent_vis
-mv ./kibana-plugin-metric-percent/* /tmp/kibana/metric_percent_vis/
-zip -r metric_percent_vis.zip /tmp/kibana
+cd /tmp && mkdir -p ./kibana/metric_percent_vis
+mv ./kibana-plugin-metric-percent/* ./kibana/metric_percent_vis/
+zip -r metric_percent_vis.zip ./kibana
+cd $BASE && mv /tmp/metric_percent_vis.zip .
 
 # Cleanup
 info "Cleanup..."
